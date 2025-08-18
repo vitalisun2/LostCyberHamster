c313a486de192d0476bdd82a12749f204a72171b
Udenkov Vitaly <VUdenkov@egartech.ru>
2025-08-18 14:06:35 +0300

    fix(GameEventSystemController): гарантируем единый EventSystem
    * Автосоздание при отсутствии, выбор подходящего модуля ввода
    * Сохраняем глобальный `EventSystem`, удаляем дубликаты, добавлены логи

--- LostCyberHamster/Assets/Scripts/GameEngine/GameEventSystemController.cs
+++ LostCyberHamster/Assets/Scripts/GameEngine/GameEventSystemController.cs
@@ -1,15 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using Assets.Scripts.System;
+﻿using Assets.Scripts.System;
 using GameAds;
 using UnityEngine;
 using Vues.GameCore;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+#if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
+using UnityEngine.InputSystem.UI;
+#endif
 
 public class GameEventSystemController : MonoBehaviour
 {
+    private static GameObject _globalEventSystem;
+
     void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+        EnsureSingleEventSystem();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        EnsureSingleEventSystem();
     }
 
     void OnEnable()
@@ -34,5 +53,58 @@ public class GameEventSystemController : MonoBehaviour
         MoneyStorage.OnDisable();
         CrystalStorage.OnDisable();
         VibrationManager.OnDisable();
+    }
+
+    private static void EnsureSingleEventSystem()
+    {
+        var all = Object.FindObjectsOfType<EventSystem>(true);
+
+        if (all.Length == 0)
+        {
+            var go = new GameObject("EventSystem", typeof(EventSystem));
+#if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
+            go.AddComponent<InputSystemUIInputModule>();
+            Debug.Log("[UI] EventSystem создан (InputSystemUIInputModule).");
+#else
+        go.AddComponent<StandaloneInputModule>();
+        Debug.Log("[UI] EventSystem создан (StandaloneInputModule).");
+#endif
+            _globalEventSystem = go;
+            Object.DontDestroyOnLoad(go);
+            return;
+        }
+
+        // выбрать кого оставить (предпочтение модулю текущей input-системы)
+#if ENABLE_INPUT_SYSTEM && !ENABLE_LEGACY_INPUT_MANAGER
+        EventSystem keep = all.FirstOrDefault(e => e.GetComponent<InputSystemUIInputModule>() != null) ?? all[0];
+#else
+    EventSystem keep = all.FirstOrDefault(e => e.GetComponent<StandaloneInputModule>() != null) ?? all[0];
+#endif
+
+        // если уже есть глобальный — предпочесть его
+        if (_globalEventSystem != null)
+        {
+            var globalEs = _globalEventSystem.GetComponent<EventSystem>();
+            if (globalEs != null) keep = globalEs;
+            else _globalEventSystem = keep.gameObject;
+        }
+        else
+        {
+            _globalEventSystem = keep.gameObject;
+        }
+
+        // удалить лишние
+        int removed = 0;
+        foreach (var es in all)
+        {
+            if (es != keep)
+            {
+                Object.Destroy(es.gameObject);
+                removed++;
+            }
+        }
+
+        Object.DontDestroyOnLoad(keep.gameObject);
+        Debug.Log($"[UI] EventSystem унифицирован. Оставлен: {keep.name}. Удалено лишних: {removed}. Модуль: {keep.currentInputModule?.GetType().Name}");
     }
 }
