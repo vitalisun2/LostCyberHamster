5406b8128f8b1d829b03386d561707f3ed1b6ab7
Udenkov Vitaly <VUdenkov@egartech.ru>
2025-10-12 19:40:27 +0300

    Use AddressableLoader for screen backgrounds

--- LostCyberHamster/Assets/Scripts/UI/Common/ScreenController.cs
+++ LostCyberHamster/Assets/Scripts/UI/Common/ScreenController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Assets.Scripts.System.Resources;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.UIElements;
@@ -15,6 +16,7 @@ namespace LostCyberHamster.UI
         public ScreenEnum Type => _screenAssetName;
 
         protected VisualElement _contentRoot;
+        private AddressableLease<Sprite> _backgroundSpriteLease;
 
         protected ScreenController(UIDocument uiDocument)
         {
@@ -39,12 +41,41 @@ namespace LostCyberHamster.UI
         public void UnsubscribeFromEvents()
         {
             OnUnsubscribeFromEvents();
+            ReleaseBackgroundSprite();
         }
 
         protected async Task ChangeBackgroundAsync(string backgroundAssetName)
         {
-            var asset = await Addressables.LoadAssetAsync<Texture2D>(backgroundAssetName).Task;
-            _background.style.backgroundImage = asset;
+            AddressableLease<Sprite> lease = null;
+            var previousLease = _backgroundSpriteLease;
+            try
+            {
+                lease = await AddressableLoader.LoadAssetAsync<Sprite>(backgroundAssetName);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"[UI-BG] Failed to load sprite for key '{backgroundAssetName}'. Details: {ex.Message}");
+                lease?.Dispose();
+                return;
+            }
+
+            var sprite = lease?.Value;
+            if (sprite == null)
+            {
+                Debug.LogError($"[UI-BG] Sprite FAILED to load for key '{backgroundAssetName}'");
+                lease?.Dispose();
+                return;
+            }
+
+            _backgroundSpriteLease = lease;
+            _background.style.backgroundImage = new StyleBackground(sprite);
+            previousLease?.Dispose();
+        }
+
+        protected void ReleaseBackgroundSprite()
+        {
+            _backgroundSpriteLease?.Dispose();
+            _backgroundSpriteLease = null;
         }
         protected abstract Task OnLoadAsync();
         protected abstract void OnSubscribeToEvents();
